b84e6e30c7ca92448a9374cd83694cf3617bf311
top0329 <top000329@gmail.com>
2024-04-10 15:02:08 +0700

    feat: blueprint modal

---
+++ config/mintPriceUnitType.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ETH: 0,
+  USDT: 1,
+  USDC: 2,
+};
---
+++ models/Blueprint.js
@@ -0,0 +1,114 @@
+const mongoose = require('mongoose');
+
+const mintPriceUnitType = require('../config/mintPriceUnitType');
+
+const Schema = mongoose.Schema;
+
+const BlueprintSchema = new Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      maxlength: [20, 'Name cannot exceed 20 characters'],
+      required: true,
+    },
+    imageUri: {
+      type: String,
+      default:
+        'https://ipfs.io/ipfs/bafkreiac47exop4qnvi47azogyp2xrb45dlyqgsijpnsvkvizkh4rm3uvi',
+      required: true,
+    },
+    creator: {
+      type: String,
+      required: true,
+    },
+    totalSupply: {
+      type: Number,
+      required: true,
+    },
+    mintPrice: {
+      type: Number,
+      required: true,
+    },
+    mintPriceUnit: {
+      type: Number,
+      enum: Object.keys(mintPriceUnitType),
+      required: true,
+    },
+    mintLimit: {
+      type: Number,
+      required: true,
+    },
+    mintedAmount: {
+      type: Number,
+      required: true,
+    },
+    data: {
+      erc20Data: [
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          tokenAddress: {
+            type: String,
+            required: true,
+          },
+          amount: {
+            type: Number,
+            required: true,
+          },
+          uri: {
+            type: String,
+            default:
+              'https://ipfs.io/ipfs/bafybeigzqwt7uavnlrj3nq44hyoicf3jcbfxi2iih6uaguj3za5t3aqxoi',
+            required: true,
+          },
+        },
+      ],
+      erc721Data: [
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          tokenAddress: {
+            type: String,
+            required: true,
+          },
+          tokenId: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+      erc1155Data: [
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          tokenAddress: {
+            type: String,
+            required: true,
+          },
+          tokenId: {
+            type: Number,
+            required: true,
+          },
+          amount: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('blueprint', BlueprintSchema);
